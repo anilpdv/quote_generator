3fffe3c0ce8173b4d971eb3d3b0719e6f34c3f42
anilpdv <pdvanil007@gmail.com>
2019-10-12 21:11:43 +0530

    adding everything

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quote from './components/Quote'
+import './App.css';
+
+function App() {
+  return (<div className="App" >
+    <Quote />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quote.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import './Quote.css'
+const Quote = props => {
+  const [quote, setQuote] = useState({});
+  const [loading, setLoading] = useState(
+
+    false
+  )
+  const [alert, setAlert] = useState(false)
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = async () => {
+    try {
+      setLoading(true)
+      const res = await fetch('http://www.quotesapi.ml/random');
+      const data = await res.json()
+      setLoading(false)
+      setQuote(data)
+
+    } catch (err) {
+      console.log(err)
+      setAlert(true)
+    }
+  };
+
+  const changeQuote = (e) => {
+    e.preventDefault()
+    getQuote();
+  }
+
+  return (<div className="Quote">
+    <div className="container">
+      <div className="">
+        {
+          loading ?
+
+            <div style={{ position: 'fixed', top: '20%', left: '50%' }} class="spinner-grow text-secondary" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>
+            :
+            (
+              <div>
+                <p className="quote p-5 text-muted">{quote.quote} <span className="author">   -  {quote.author}</span></p>
+                <button onClick={changeQuote} className="btn btn-success "> quote</button>
+              </div>
+            )
+        }
+      </div>
+
+    </div>
+  </div>);
+};
+
+export default Quote;
